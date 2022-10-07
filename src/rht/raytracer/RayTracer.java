c0073a0556570fe6a8d4786eae4539a56f39d53c
Heather Tweedie <rhtweedie12@gmail.com>
2022-10-07 22:07:19 +0100

    Make an image we can draw to

--- src/rht/raytracer/RayTracer.java
+++ src/rht/raytracer/RayTracer.java
@@ -1,6 +1,28 @@
 package rht.raytracer;
 
-public class RayTracer {
-    public static void main(String[] args) {
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.image.BufferedImage;
+
+public class RayTracer extends JPanel {
+    private static final int WIDTH = 400;
+    private static final int HEIGHT = 400;
+
+    private final BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+
+    public RayTracer() {
+        ImageIcon icon = new ImageIcon(image);
+        add(new JLabel(icon));
     }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Ray tracer");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(new RayTracer());
+        frame.pack();
+        frame.setVisible(true);
+    }
+
 }
