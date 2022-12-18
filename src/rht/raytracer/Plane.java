b7a12444fd76927381c506558c774dad260774e5
Heather Tweedie <rhtweedie12@gmail.com>
2022-12-18 23:55:25 +0000

    Add Planes

---
+++ src/rht/raytracer/Plane.java
@@ -0,0 +1,39 @@
+package rht.raytracer;
+
+public class Plane implements Shape {
+
+    private final Vec3 centre;
+    private final Vec3 normal;
+    private final Colour colour;
+
+    public Plane(Vec3 centre, Vec3 normal, Colour colour) {
+        this.centre = centre;
+        this.normal = normal;
+        this.colour = colour;
+    }
+
+    @Override
+    public Double intersect(Ray ray) {
+        double denominator = normal.dot(ray.getDirection());
+        double numerator = normal.dot(centre.minus(ray.getOrigin()));
+        if (denominator == 0) {
+            return null;
+        }
+        double distance = numerator / denominator;
+        if (distance < 0) {
+            return null;
+        }
+        return distance;
+    }
+
+    @Override
+    public Vec3 normalAtPoint(Vec3 pointOnSurface) {
+        return normal;
+    }
+
+    @Override
+    public Colour getColour() {
+        return colour;
+    }
+
+}
