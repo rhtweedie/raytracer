294e014ddc290c3531c593562e4a01482340ce07
Heather Tweedie <rhtweedie12@gmail.com>
2022-12-24 15:54:28 +0000

    Create tests for plane intersection

---
+++ src/rht/raytracer/PlaneTest.java
@@ -0,0 +1,23 @@
+package rht.raytracer;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class PlaneTest {
+
+    @Test
+    public void testIntersect() {
+        Plane plane = new Plane(new Vec3(0, 0, 0), new Vec3(0, -1, 0), new Colour(1, 1, 1));
+
+        double distancePerpendicular = plane.intersect(new Ray(new Vec3(0, -5, 0), new Vec3(0, 1, 0)));
+        assertEquals(5, distancePerpendicular, 0.000001);
+
+        Double distanceParallel = plane.intersect((new Ray(new Vec3(1, -1, 2), new Vec3(1, 0, 0))));
+        assertEquals(null, distanceParallel);
+
+        Double distanceAway = plane.intersect((new Ray(new Vec3(1, -1, 2), new Vec3(1, -1, -1))));
+        assertEquals(null, distanceAway);
+
+    }
+}
