75f14d8d318d9c6ea1bac145c8e4970cc29c77db
Heather Tweedie <rhtweedie12@gmail.com>
2023-01-26 22:42:35 +0000

    Fix order for assertEquals

--- app/src/test/java/rht/raytracer/maths/MatrixTest.java
+++ app/src/test/java/rht/raytracer/maths/MatrixTest.java
@@ -10,8 +10,8 @@ public class MatrixTest {
         Matrix a = new Matrix(new double[][] { new double[] { 1, 0 }, new double[] { 0, 1 } });
         Matrix b = new Matrix(new double[][] { new double[] { 2, 3 }, new double[] { 4, 5 } });
 
-        assertEquals(a.times(b), b);
-        assertEquals(b.times(a), b);
+        assertEquals(b, a.times(b));
+        assertEquals(b, b.times(a));
     }
 
     @Test
@@ -22,8 +22,8 @@ public class MatrixTest {
         Matrix expectedAB = new Matrix(new double[][] { new double[] { 19, 22 }, new double[] { 43, 50 } });
         Matrix expectedBA = new Matrix(new double[][] { new double[] { 23, 34 }, new double[] { 31, 46 } });
 
-        assertEquals(a.times(b), expectedAB);
-        assertEquals(b.times(a), expectedBA);
+        assertEquals(expectedAB, a.times(b));
+        assertEquals(expectedBA, b.times(a));
     }
 
     @Test
@@ -32,6 +32,6 @@ public class MatrixTest {
         Matrix inverseA = a.inverse();
         Matrix identity = new Matrix(new double[][] { new double[] { 1, 0 }, new double[] { 0, 1 } });
 
-        assertEquals(inverseA.times(a), identity);
+        assertEquals(identity, inverseA.times(a));
     }
 }
