811b6ad9cdccdb338ed92897c663f2ec041ddf70
Heather Tweedie <rhtweedie12@gmail.com>
2023-03-27 19:59:30 +0100

    Add test for rotated plane

--- app/src/test/java/rht/raytracer/shapes/TransformedTest.java
+++ app/src/test/java/rht/raytracer/shapes/TransformedTest.java
@@ -1,6 +1,7 @@
 package rht.raytracer.shapes;
 
 import static org.junit.Assert.assertEquals;
+import static rht.raytracer.maths.AssertHelpers.assertVecEquals;
 
 import org.junit.Test;
 
@@ -20,5 +21,19 @@ public class TransformedTest {
         Ray ray = new Ray(new Vec3(1, -1, 0), new Vec3(0, 1, 0));
         double intersectionDistance = transformedSphere.intersect(ray);
         assertEquals(intersectionDistance, 2.0, EPSILON);
+    }
+
+    @Test
+    public void testRotatedPlane() {
+        Plane plane = new Plane(new Vec3(0, 0, 0), new Vec3(0, 1, 0));
+        Transformed rotatedPlane = new Transformed(plane, Matrix.rotateZ(45));
+
+        Ray ray = new Ray(new Vec3(-1, 1, 0), new Vec3(0, -1, 0));
+        double intersectionDistance = rotatedPlane.intersect(ray);
+        assertEquals(intersectionDistance, 2.0, EPSILON);
+
+        Vec3 intersectionPoint = ray.distanceAlong(intersectionDistance);
+        Vec3 normal = rotatedPlane.normalAtPoint(intersectionPoint);
+        assertVecEquals(normal, new Vec3(-1, 1, 0).normalise(), EPSILON);
     }
 }
