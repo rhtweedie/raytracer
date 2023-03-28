eafc6a5b8e8bf9e2560d2dcbb0d2527ed69a4445
Heather Tweedie <rhtweedie12@gmail.com>
2023-03-28 23:12:35 +0100

    WIP on loading scene from YAML file

--- app/src/main/java/rht/raytracer/RayTracer.java
+++ app/src/main/java/rht/raytracer/RayTracer.java
@@ -5,16 +5,23 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import org.yaml.snakeyaml.Yaml;
+
 import rht.raytracer.maths.Matrix;
 import rht.raytracer.maths.Vec3;
 import rht.raytracer.shapes.Plane;
 import rht.raytracer.shapes.Shape;
+import rht.raytracer.shapes.ShapeType;
 import rht.raytracer.shapes.Sphere;
 import rht.raytracer.shapes.Transformed;
 
 import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class RayTracer extends JPanel {
     private static final int WIDTH = 1000;
@@ -32,48 +39,72 @@ public class RayTracer extends JPanel {
     }
 
     public static void main(String[] args) {
-        List<Shape> objects = new ArrayList<>();
-        List<Light> lights = new ArrayList<>();
-        // Medium spheres
-        objects.add(new Shape(new Sphere(new Vec3(-1.0, -1.0, 5.0), 1.0), new Colour(1.0, 0.9, 0.9),
-                new Colour(0.2, 0.2, 0.2)));
-        Sphere sphere = new Sphere(new Vec3(0, 0, 0.0), 0.5);
-        Transformed longSphere = new Transformed(sphere,
-                Matrix.translation(0.5, -0.5, 0).times(Matrix.scale(1, 3, 1)));
-        objects.add(new Shape(longSphere, new Colour(1.0, 0.5, 0.0)));
-
-        // Little spheres
-        for (double x = -1.6; x <= -0.5; x += 0.3) {
-            for (double z = 0.0; z <= 2.0; z += 0.3) {
-                objects.add(new Shape(new Sphere(new Vec3(x, 0.5, z), 0.04), new Colour(0.5, 0.5, 1.0)));
-            }
+        if (args.length != 1) {
+            System.err.println("Usage:");
+            System.err.println("  RayTracer <Scene.yaml>");
+            System.exit(1);
         }
-        // Bottom plane
-        objects.add(
-                new Shape(new Plane(new Vec3(0.0, 1.02, 0.0), new Vec3(0.0, -1.0, 0.0)), new Colour(0.8, 0.6, 0.8),
-                        new Colour(0.7, 0.7, 0.7)));
-        // Right plane
-        objects.add(
-                new Shape(new Plane(new Vec3(1.5, 0.0, 0.0), new Vec3(-1.0, 0.0, 0.0)),
-                        Colour.WHITE));
-        // Back plane
-        objects.add(new Shape(new Plane(new Vec3(0.0, 0.0, 10.0), new Vec3(0.0, 0.0, -1.0)), Colour.WHITE));
-        // Lights
-        lights.add(new Light(new Vec3(0.0, -5.0, -5.0), Colour.WHITE));
-        lights.add(new Light(new Vec3(-1.0, 0.7, 1.0), new Colour(0.2, 0.15, 0.2)));
-        Scene scene = new Scene(objects, lights);
 
-        Camera camera = new Camera(new Vec3(0.0, 0.0, -5.0), new Vec3(0.0, 0.0, -2.0), new Vec3(1.0, 0.0, 0.0),
-                new Vec3(0.0, 1.0, 0.0));
+        RayTracer rayTracer;
+        try {
+            rayTracer = RayTracer.loadYAML(args[0]);
+        } catch (FileNotFoundException e) {
+            System.err.println(args[0] + " not found.");
+            System.exit(2);
+            return;
+        }
 
         JFrame frame = new JFrame("Ray tracer");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        RayTracer rayTracer = new RayTracer(scene, camera);
         rayTracer.render();
         frame.add(rayTracer);
         frame.pack();
         frame.setVisible(true);
+    }
 
+    private static RayTracer loadYAML(String filename) throws FileNotFoundException {
+        List<Shape> objects = new ArrayList<>();
+        List<Light> lights = new ArrayList<>();
+
+        Yaml yaml = new Yaml();
+        InputStream file = new FileInputStream(filename);
+        Map<String, Object> contents = yaml.load(file);
+        List<Object> yamlObjects = (List<Object>) contents.get("objects");
+        List<Object> yamlLights = (List<Object>) contents.get("lights");
+        Map<String, Object> yamlCamera = (Map<String, Object>) contents.get("camera");
+        System.out.println("Objects: " + yamlObjects);
+        System.out.println("Lights: " + yamlLights);
+        System.out.println("Camera: " + yamlCamera);
+
+        for (Object o : yamlObjects) {
+            Map<String, Object> object = (Map<String, Object>) o;
+            String shapeName = (String) object.get("shape");
+            ShapeType shapeType;
+            switch (shapeName) {
+                case "Sphere":
+                    List<Float> centreList = (List<Float>) object.get("centre");
+                    if (centreList.size() != 3) {
+                        throw new IllegalArgumentException("sphere centre had unexpected length " + centreList.size());
+                    }
+                    Vec3 centre = new Vec3(centreList.get(0), centreList.get(1), centreList.get(2));
+                    double radius = (Float) object.get("radius");
+                    shapeType = new Sphere(centre, radius);
+                    break;
+                case "Plane":
+                default:
+                    throw new IllegalArgumentException("Unexpected shape " + shapeName);
+            }
+            List<Float> colourList = (List<Float>) object.get("colour");
+            if (colourList.size() != 3) {
+                throw new IllegalArgumentException("Colour had unexpected length " + colourList.size());
+            }
+            Colour colour = new Colour(colourList.get(0), colourList.get(1), colourList.get(2));
+            objects.add(new Shape(shapeType, colour));
+        }
+        Scene scene = new Scene(objects, lights);
+        Camera camera = new Camera(new Vec3(0.0, 0.0, -5.0), new Vec3(0.0, 0.0, -2.0), new Vec3(1.0, 0.0, 0.0),
+                new Vec3(0.0, 1.0, 0.0));
+        return new RayTracer(scene, camera);
     }
 
     private void render() {
