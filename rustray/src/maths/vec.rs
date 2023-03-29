e255c0c07e8b178618d99bbdf950de8ae1b314b8
Andrew Walbran <qwandor@gmail.com>
2023-03-29 23:02:22 +0100

    Scalar multiplication and division for Vector, and normalisation.

--- rustray/src/maths/vec.rs
+++ rustray/src/maths/vec.rs
@@ -1,4 +1,4 @@
-use std::ops::{Add, Mul, Sub};
+use std::ops::{Add, Div, Mul, Sub};
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct Vector<const LENGTH: usize>([f64; LENGTH]);
@@ -10,6 +10,11 @@ impl<const LENGTH: usize> Vector<LENGTH> {
 
     pub fn length(&self) -> f64 {
         self.squared().sqrt()
+    }
+
+    /// Returns a unit vector pointing in the same direction as this one.
+    pub fn normalise(&self) -> Self {
+        self / self.length()
     }
 }
 
@@ -73,6 +78,46 @@ impl<const LENGTH: usize> Mul for Vector<LENGTH> {
     }
 }
 
+impl<const LENGTH: usize> Mul<f64> for &Vector<LENGTH> {
+    type Output = Vector<LENGTH>;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        let mut result = self.0.clone();
+        for i in 0..LENGTH {
+            result[i] *= rhs;
+        }
+        Vector(result)
+    }
+}
+
+impl<const LENGTH: usize> Mul<f64> for Vector<LENGTH> {
+    type Output = Vector<LENGTH>;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        &self * rhs
+    }
+}
+
+impl<const LENGTH: usize> Div<f64> for &Vector<LENGTH> {
+    type Output = Vector<LENGTH>;
+
+    fn div(self, rhs: f64) -> Self::Output {
+        let mut result = self.0.clone();
+        for i in 0..LENGTH {
+            result[i] /= rhs;
+        }
+        Vector(result)
+    }
+}
+
+impl<const LENGTH: usize> Div<f64> for Vector<LENGTH> {
+    type Output = Vector<LENGTH>;
+
+    fn div(self, rhs: f64) -> Self::Output {
+        &self / rhs
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -105,5 +150,25 @@ mod tests {
         let b = Vector([5.0, -4.0, -1.0]);
         assert_eq!(&a * &b, -1.0);
         assert_eq!(a * b, -1.0);
+    }
+
+    #[test]
+    fn scalar_multiply() {
+        let v = Vector([5.0, -4.0, -1.0]);
+        assert_eq!(&v * 3.0, Vector([15.0, -12.0, -3.0]));
+        assert_eq!(v * 3.0, Vector([15.0, -12.0, -3.0]));
+    }
+
+    #[test]
+    fn scalar_divide() {
+        let v = Vector([5.0, -4.0, -1.0]);
+        assert_eq!(&v / 2.0, Vector([2.5, -2.0, -0.5]));
+        assert_eq!(v / 2.0, Vector([2.5, -2.0, -0.5]));
+    }
+
+    #[test]
+    fn normalise() {
+        let v = Vector([3.0, -4.0, 0.0]);
+        assert_eq!(v.normalise(), Vector([0.6, -0.8, 0.0]));
     }
 }
