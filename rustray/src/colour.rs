302993b9fa9476ccbd871a472be13c3da42714f3
Andrew Walbran <qwandor@gmail.com>
2023-04-05 00:47:14 +0100

    Add camera and render scene.

--- rustray/src/colour.rs
+++ rustray/src/colour.rs
@@ -14,6 +14,14 @@ impl Colour {
     pub const fn new(r: f64, g: f64, b: f64) -> Self {
         Self { r, g, b }
     }
+
+    pub fn as_rgb(&self) -> [u8; 3] {
+        [
+            (self.r * 255.0) as u8,
+            (self.g * 255.0) as u8,
+            (self.b * 255.0) as u8,
+        ]
+    }
 }
 
 impl Add for &Colour {
--- rustray/src/main.rs
+++ rustray/src/main.rs
@@ -1,3 +1,4 @@
+mod camera;
 mod colour;
 mod maths;
 mod ray;
@@ -11,6 +12,9 @@ use crate::{
     scene::{Light, Object, Scene},
     shapes::sphere::Sphere,
 };
+
+const WIDTH: u32 = 1000;
+const HEIGHT: u32 = 1000;
 
 fn main() {
     let scene = Scene {
@@ -33,4 +37,26 @@ fn main() {
             },
         ],
     };
+    let camera = Camera {
+        focal_point: Vector([0.0, 0.0, -5.0]),
+        frame_centre: Vector([0.0, 0.0, -2.0]),
+        x_direction: Vector([1.0, 0.0, 0.0]),
+        y_direction: Vector([0.0, 1.0, 0.0]),
+    };
+    let image = render(&scene, &camera, WIDTH, HEIGHT);
+}
+
+fn render(scene: &Scene, camera: &Camera, width: u32, height: u32) -> Vec<u8> {
+    let mut image = Vec::new();
+    for y in 0..height {
+        for x in 0..width {
+            let ray = camera.ray_for_pixel(
+                x as f64 * 2.0 / width as f64 - 1.0,
+                y as f64 * 2.0 / height as f64 - 1.0,
+            );
+            let pixel = scene.colour_for_ray(&ray);
+            image.extend(pixel.as_rgb());
+        }
+    }
+    image
 }
